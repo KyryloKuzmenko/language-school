c8fc691b6f86cf5a8711f6583a5ffd6ad3a61c9d
Kyrylko <kkuzmenko1992@gmail.com>
2024-09-04 13:40:11 -0230

    create sections: header(navigate, address), hero(text, picture with vectors)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header'
+
+import './App.css'
+import Hero from './components/Hero/Hero'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
