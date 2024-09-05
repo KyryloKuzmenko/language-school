e62bcaef2ea2f8f7869ed90f9058e18b02e6692c
Kyrylko <kkuzmenko1992@gmail.com>
2024-09-05 00:25:27 -0230

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,36 +19,36 @@ const Hero = () => {
       </div>
       <div className={`${style.heroPictureContainer} `}>
         <svg width="110" height="262" className={style.vector1}>
-          <use href="src/assets/icons/vectors.svg#vector1"></use>
+          <use href="/src/assets/icons/vectors.svg#vector1"></use>
         </svg>
         <svg width="49" height="212" className={style.vector2}>
-          <use href="src/assets/icons/vectors.svg#vector2"></use>
+          <use href="/src/assets/icons/vectors.svg#vector2"></use>
         </svg>
         <svg width="265" height="261" className={style.vector3}>
-          <use href="src/assets/icons/vectors.svg#vector3"></use>
+          <use href="/src/assets/icons/vectors.svg#vector3"></use>
         </svg>
         <svg width="120" height="171" className={style.vector4}>
-          <use href="src/assets/icons/vectors.svg#vector4"></use>
+          <use href="/src/assets/icons/vectors.svg#vector4"></use>
         </svg>
         <svg width="106" height="112" className={style.vector5}>
-          <use href="src/assets/icons/vectors.svg#vector5"></use>
+          <use href="/src/assets/icons/vectors.svg#vector5"></use>
         </svg>
         <svg width="230" height="240" className={style.vector6}>
-          <use href="src/assets/icons/vectors.svg#vector6"></use>
+          <use href="/src/assets/icons/vectors.svg#vector6"></use>
         </svg>
         <svg width="464" height="430" className={style.vector7}>
-          <use href="src/assets/icons/vectors.svg#vector7"></use>
+          <use href="/src/assets/icons/vectors.svg#vector7"></use>
         </svg>
         <svg width="195" height="224" className={style.vector8}>
-          <use href="src/assets/icons/vectors.svg#vector8"></use>
+          <use href="/src/assets/icons/vectors.svg#vector8"></use>
         </svg>
         <img
           className={style.heroImg}
           srcSet="
-            src/assets/img/x2/girl-person@2x.png 2x,
-            src/assets/img/girl-person.png
+            /src/assets/img/x2/girl-person@2x.png 2x,
+            /src/assets/img/girl-person.png
             "
-          src="src/assets/img/girl-person.png"
+          src="/src/assets/img/girl-person.png"
           alt="Girl Photo"
         />
       </div>
