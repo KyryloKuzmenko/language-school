6836b765be7136337c3008996e5b1e68bcba84f9
Kyrylko <kkuzmenko1992@gmail.com>
2024-09-14 21:18:13 -0230

    fixed hero section, added picture and bground for PC

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,20 +4,47 @@ const Hero = () => {
   return (
     <section className={`${style.heroSection} container`}>
       <div className={style.wrapHero}>
-        <a className={style.linkJoinUs} href="#join-us">
-          Join Us
-        </a>
-        <h1 className={style.heroTitle}>HIGH QUALITY COURSES </h1>
-        <div className={`${style.heroPictureContainer}`}>
+        <div>
+          <a className={style.linkJoinUs} href="#join-us">
+            Join Us
+          </a>
+          <h1 className={style.heroTitle}>HIGH QUALITY COURSES </h1>
+          <div className={`${style.heroPictureContainer}`}>
+            <img
+              className={style.bground}
+              src="/assets/img/background-removebg-preview.png"
+              alt="background"
+              width="350"
+              height="300"
+            />
+            <img
+              className={style.heroImg}
+              srcSet="
+                /assets/img/x2/girl-person@2x.png 2x,
+                /assets/img/girl-person.png
+                "
+              src="/assets/img/girl-person.png"
+              alt="Girl Photo"
+            />
+          </div>
+          <p className={style.text}>
+            Every day brings with it a fresh set of learning possibilities.
+          </p>
+          <div className={style.btnWrap}>
+            <button className={style.joinUs}>Join Us</button>
+            <button className={style.learnMore}>Learn More</button>
+          </div>
+        </div>
+        <div className={style.heroPictureWrap}>
           <img
-            className={style.bground}
+            className={style.bgroundPC}
             src="/assets/img/background-removebg-preview.png"
             alt="background"
-            width="350"
-            height="300"
+            // width="570"
+            // height="470"
           />
           <img
-            className={style.heroImg}
+            className={style.heroImgPC}
             srcSet="
               /assets/img/x2/girl-person@2x.png 2x,
               /assets/img/girl-person.png
@@ -25,14 +52,6 @@ const Hero = () => {
             src="/assets/img/girl-person.png"
             alt="Girl Photo"
           />
-        </div>
-        <p className={style.text}>
-          Every day brings with it a fresh set of learning possibilities.
-        </p>
-
-        <div className={style.btnWrap}>
-          <button className={style.joinUs}>Join Us</button>
-          <button className={style.learnMore}>Learn More</button>
         </div>
       </div>
     </section>
